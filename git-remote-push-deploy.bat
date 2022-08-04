a203d55aa940130559f3916338c002364b9895f9
Adam Childers <adam@clueresearch.com>
2022-08-04 09:01:11 -0500

    data server v1.0

--- git-remote-push-deploy.bat
+++ git-remote-push-deploy.bat
@@ -3,6 +3,6 @@ git remote add gandi ssh+git://6919303@git.sd3.gpaas.net/default.git
 git push gandi master
 ssh 6919303@git.sd3.gpaas.net deploy default.git
 git remote rm gandi
-git remote add github https://github.com/semiographics/rhizomatic-django.git
+git remote add github https://github.com/semiographics/clue-django.git
 git push github master
 git remote rm github
