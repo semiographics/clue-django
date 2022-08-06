07af193deda57921b17964f9c34858daf99fb0fc
Adam Childers <adam@clueresearch.com>
2022-08-06 11:52:38 -0500

    api v1.0

--- main/settings.py
+++ main/settings.py
@@ -35,8 +35,7 @@ ALLOWED_HOSTS = ['.clueresearch.com', '127.0.0.1', 'localhost']
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 # Application definition
