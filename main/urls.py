594f4638d96e4f5497de7d91913a377b68c26825
Adam Childers <adam@clueresearch.com>
2022-08-06 12:01:01 -0500

    api v1.0

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # path('', views.snippet_list),
@@ -7,4 +8,6 @@ urlpatterns = [
     # path('<int:pk>/', views.snippet_detail),
     # path('<int:pk>/', views.TaxiDetail.as_view()),
     # path('', views.OcafList.as_view()),
+    path('mike/', TemplateView.as_view(template_name="main/mike.html")), ##views.index, name='index'
+
 ]
