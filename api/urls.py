124646651e2348bda2e4c667c194785577a331f3
Adam Childers <adam@clueresearch.com>
2022-08-06 12:47:40 -0500

    api v1.0

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('<int:pk>/', views.snippet_detail),
     # path('<int:pk>/', views.TaxiDetail.as_view()),
     path('ocaf', views.OcafList.as_view()),
-    path('mike/', TemplateView.as_view(template_name="ocaf/ocaf.html")), ##views.index, name='index'
+    path('mike/', TemplateView.as_view(template_name="main/templates/ocaf/ocaf.html")), ##views.index, name='index'
 
 ]
 
