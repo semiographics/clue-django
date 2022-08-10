4bdefbbc18b1d8d3d622af8069e8a20e5a4f830d
Adam Childers <adam@clueresearch.com>
2022-08-10 17:26:40 -0500

    api v1.0

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from api.models import Ocaf
 from api.serializers import OcafSerializer
+from rest_framework_csv import renderers as r
+
 
 # class TaxiList(generics.ListAPIView):
 #     queryset = ChicagoTaxi.objects.all()
@@ -11,9 +13,10 @@ from api.serializers import OcafSerializer
 #     filter_backends = [DjangoFilterBackend]
 #     filterset_fields = ['trip_id','taxi_id']
 
-class OcafList(generics.ListAPIView):
+class OperatingCostAdjustmentFactors(generics.ListAPIView):
     queryset = Ocaf.objects.all()
     serializer_class = OcafSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = {'year': ["in", "gte", "gt", "lte", "lt", "range"],
                         'state': ["in", "iexact", "istartswith", "icontains"]}
+    renderer_classes = (r.CSVRenderer, ) + tuple(api_settings.)
