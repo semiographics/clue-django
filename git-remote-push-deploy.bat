44ea319982a1e0bdbf48335b36cbdc640dd1a1b7
Adam Childers <adam@clueresearch.com>
2022-08-11 15:27:15 -0500

    api v1.0

--- git-remote-push-deploy.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-git remote add gandi ssh+git://6919303@git.sd3.gpaas.net/default.git
-git push gandi master
-ssh 6919303@git.sd3.gpaas.net deploy default.git
-git remote rm gandi
-git remote add github https://github.com/semiographics/clue-django.git
-git push github master
-git remote rm github
